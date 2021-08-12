6c6b8e13dc6476842f951e311e0ade465e3c2062
Evandro Ladislau <evandroladislau23@gmail.com>
2021-08-11 22:31:35 -0300

    Modificação arquivos de controle, repository e dto

--- src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
+++ src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
@@ -1,17 +1,32 @@
 package one.digitalinnovation.personapi.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import one.digitalinnovation.personapi.dto.response.MessageResponseDTO;
+import one.digitalinnovation.personapi.entity.Person;
+import one.digitalinnovation.personapi.repository.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController
 @RequestMapping("/api/v1/people")
 public class PersonController {
 
-    @GetMapping
-    public String getBook(){
+    //criou um atributo da interface PersonRepository
+    private PersonRepository personRepository;
 
-        return "API Test!";
+    //injecao de dependencia
+    @Autowired //indica para inspring injetar uma implementacao do tipo repository
+    public PersonController(PersonRepository personRepository) {
+        this.personRepository = personRepository;
+    }
+
+    //criar um livro por isso usa post
+    @PostMapping
+    public MessageResponseDTO createPerson(@RequestBody Person person){
+        Person savePerson = personRepository.save(person);
+        return MessageResponseDTO
+                .builder()
+                .message("Created person with ID" + savePerson.getId())
+                .build();
     }
 }
