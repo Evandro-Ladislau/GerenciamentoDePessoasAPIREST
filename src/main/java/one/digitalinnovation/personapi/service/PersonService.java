84cfec6c4ada5c331b7161d0561b19d86671139d
Evandro Ladislau <evandroladislau23@gmail.com>
2021-08-13 00:12:23 -0300

    Developed list all method

--- src/main/java/one/digitalinnovation/personapi/service/PersonService.java
+++ src/main/java/one/digitalinnovation/personapi/service/PersonService.java
@@ -8,6 +8,9 @@ import one.digitalinnovation.personapi.repository.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 
 //ele indica para o spring que sera gerenciada uma classe com as regras de negocio.
 @Service
@@ -32,5 +35,12 @@ public class PersonService {
                 .builder()
                 .message("Created person with ID" + savedPerson.getId())
                 .build();
+    }
+
+    public List<PersonDTO> listAll() {
+        List<Person> allPeople = personRepository.findAll();
+        return allPeople.stream()
+                .map(personMapper::toDTO)
+                .collect(Collectors.toList());
     }
 }
